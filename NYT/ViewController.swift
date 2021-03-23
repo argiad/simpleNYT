be8460b573f4b2d9ee5c5bbbd30eae5871ff803c
argiad <a.s.mkrtchyan@gmail.com>
2021-03-23 11:02:06 -0500

    Initial Commit

---
+++ NYT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYT
+//
+//  Created by Artem Mkrtchyan on 3/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
